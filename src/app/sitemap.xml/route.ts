d0588088ed93421e38acd8f957074f109e297a77
mayankmalik263 <mayankmalik263@gmail.com>
2026-05-12 11:55:52 +0530

    fix: remove leading whitespace from sitemap xml

--- src/app/sitemap.xml/route.ts
+++ src/app/sitemap.xml/route.ts
@@ -23,21 +23,18 @@ export async function GET() {
   const allRoutes = [...staticRoutes, ...blogRoutes];
 
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-      ${allRoutes
-        .map((url) => {
-          return `
-            <url>
-              <loc>${url}</loc>
-              <lastmod>${new Date().toISOString()}</lastmod>
-              <changefreq>monthly</changefreq>
-              <priority>0.8</priority>
-            </url>
-          `;
-        })
-        .join("")}
-    </urlset>
-  `;
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+${allRoutes
+  .map((url) => {
+    return `  <url>
+    <loc>${url}</loc>
+    <lastmod>${new Date().toISOString()}</lastmod>
+    <changefreq>monthly</changefreq>
+    <priority>0.8</priority>
+  </url>`;
+  })
+  .join("\n")}
+</urlset>`;
 
   return new Response(sitemap, {
     headers: {
